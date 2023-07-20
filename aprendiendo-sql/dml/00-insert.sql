4b34d71c0083f6e1a2372826407d70ce1b21486c
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-07-20 15:51:27 -0500

    Insert y update

---
+++ aprendiendo-sql/dml/00-insert.sql
@@ -0,0 +1,15 @@
+/* INSERTAR DATOS */
+
+-- METODO 1
+-- Sin el id porque es auto_increment
+INSERT INTO clientes(nombre, apellido, email, telefono, direccion, nacionalidad) VALUES
+('Mateo', 'Alvarez','mateo@correo.inventado', '+9834568943', 'Caracas', 'colombia');
+
+-- METODO 2
+INSERT INTO clientes SET nombre='Daniel', apellido='Ramirez', email='daniel@correo.inventado', telefono='+8465349856',
+direccion='New york', nacionalidad='panama';
+
+-- METODO 3
+INSERT INTO clientes(nombre, apellido, email, telefono, direccion, nacionalidad) VALUES
+('Yesica', 'Jaramillo','yesica@correo.inventado', '+238045745', 'Lima', 'peru'),
+('Kevin', 'Londoño','kevin@correo.inventado', '+3452789023', 'Buenos Aires', 'brasil');
---
+++ aprendiendo-sql/dml/02-update.sql
@@ -0,0 +1,6 @@
+/* ACTUALIZAR DATOS */
+
+UPDATE clientes SET apellido='Murillo', telefono='+98752342'
+WHERE id='1';
+
+UPDATE clientes SET telefono='+987798987' WHERE nacionalidad='Mexico';
